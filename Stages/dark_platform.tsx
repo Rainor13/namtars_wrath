a4ebb0071a2d425e8bf4490a86f124b669d0d69b
Rainor13 <79574428+Rainor13@users.noreply.github.com>
2025-01-20 15:48:20 +0100

    Delete Stages directory

--- Stages/dark_platform.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="dark_platform" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="../Images/rock wall tileset.png" trans="ffffff" width="96" height="96"/>
-</tileset>
--- Stages/skeleton.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="skeleton" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../Images/32x32_enemy.png" trans="ffffff" width="32" height="32"/>
-</tileset>
