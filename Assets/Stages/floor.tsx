81bb0784a1124ff45cb14a1b84181e6fd2a27c93
Rainor13 <79574428+Rainor13@users.noreply.github.com>
2025-01-20 15:46:45 +0100

    Add files via upload

---
+++ Assets/Stages/floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="floor" tilewidth="32" tileheight="32" tilecount="1392" columns="58">
+ <image source="../Images/forest.png" width="1856" height="793"/>
+</tileset>
---
+++ Assets/Stages/platform.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="platform" tilewidth="32" tileheight="32" margin="2" tilecount="9" columns="3">
+ <image source="../Images/pixel_art_forest.png" width="104" height="102"/>
+</tileset>
